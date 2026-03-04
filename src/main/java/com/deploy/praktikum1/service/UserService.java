bbaca398a8701ccd9ae0124660989e86dab3a489
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-03-04 11:00:21 +0700

    menyelesaikan file UserServiceImpl di dalam package impl

--- src/main/java/com/deploy/praktikum1/service/UserService.java
+++ src/main/java/com/deploy/praktikum1/service/UserService.java
@@ -6,9 +6,15 @@ import com.deploy.praktikum1.model.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
+    UserDto addUser(UserAddRequest request);
+
     UserDto AddUser(UserAddRequest request);
     List<UserDto> getAllUser();
     UserDto getUserById(String id);
     UserDto UpdateUser(String id, UserAddRequest request);
     void  DeleteUser(String id);
+
+    UserDto updateUser(String id, UserAddRequest request);
+
+    void deleteUser(String id);
 }
